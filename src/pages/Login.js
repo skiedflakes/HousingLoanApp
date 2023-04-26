4287afbc0d67ffd20c69bfcc790d673773545cd6
skiedflakes <46420286+skiedflakes@users.noreply.github.com>
2023-04-26 12:54:06 +0800

    update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -87,7 +87,7 @@ export default function SignIn() {
             </Grid>
             <Grid item>
               <Link href="#" variant="body2" onClick={handleClick}>
-                {"Don't have an account? Sign Up"}
+                {"Don't have an account? Start Register"}
               </Link>
             </Grid>
           </Grid>
